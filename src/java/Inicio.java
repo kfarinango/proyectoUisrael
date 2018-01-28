79d17094c741b20495c8f853d807766a872c6d31
kfarinango <kevinf@openmailbox.org>
2018-01-28 17:44:25 -0500

    Creamos el servlet

--- src/java/Inicio.java
+++ src/java/Inicio.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,8 +16,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author User
  */
-@WebServlet(name = "inicio", urlPatterns = {"/inicio"})
-public class inicio extends HttpServlet {
+@WebServlet(urlPatterns = {"/Inicio"})
+public class Inicio extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,10 +36,10 @@ public class inicio extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet inicio</title>");            
+            out.println("<title>Servlet Inicio</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet inicio at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Inicio at hola " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
